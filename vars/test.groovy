d001b8e83708488fe53d0cd3c4bdbb28b2ed5713
sudhfree <sudhakar.srivastava84@gmail.com>
2020-11-24 00:45:58 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,4 +1,46 @@
 def call()
 {
-echo "hi"
+
+def agent= null
+pipeline
+{
+agent any
+
+parameters{
+string(description: 'Enter the branch', name: 'branch1')
+}
+
+stages{
+stage("setting the agent")
+{
+  steps{
+  script{
+  agent = env.branch1
+  }
+  }
+}
+}
+
+}
+
+pipeline
+{
+agent any
+
+parameters{
+string(description: 'Enter the repo', name: 'repo1')
+}
+
+stages{
+stage("setting the agent")
+{
+  steps{
+  echo env.repo
+  }
+}
+}
+
+
+
+}
 }
